890528339740c0cfd65a0435906ef3be7a625f4e
looksaw <xdu2814031084@gmail.com>
2025-06-29 20:13:30 +0800

    add readme

--- src/test/java/TestSort.java
+++ src/test/java/TestSort.java
@@ -1,4 +1,15 @@
+import org.junit.jupiter.api.Test;
+import org.looksaw.Sort.SelectionSort;
+import org.looksaw.Sort.Sort;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class TestSort {
-
+    @Test
+    public void TestSelectionSort() {
+        Sort sort = new SelectionSort();
+        Integer[] arr = {5,4,3,2,1};
+        sort.sort(arr);
+        Integer[] expected = {1,2,3,4,5};
+        assertArrayEquals(arr, expected);
+    }
 }
